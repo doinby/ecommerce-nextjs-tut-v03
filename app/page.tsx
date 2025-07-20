b6cbee061e114a6b418acbbb7530413d11a0a539
Kacy Do <contact@kacydo.dev>
2025-07-20 21:05:37 +1000

    add stripe api

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-	return <h1 className='text-3xl text-teal-500'>Hello World!</h1>;
+import { stripe } from '@/lib/stripe';
+
+export default async function Home() {
+	const products = await stripe.products.list({
+		expand: ['data.default_price'],
+		limit: 5,
+	});
+	console.log(products);
+
+	return <></>;
 }
