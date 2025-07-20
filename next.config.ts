b50f904d9aa767f8110d2b218cc5a0a30d403ead
Kacy Do <contact@kacydo.dev>
2025-07-20 22:46:55 +1000

    configure image source

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'files.stripe.com',
+				port: '',
+				pathname: '/links/**',
+				search: '',
+			},
+		],
+	},
 };
 
 export default nextConfig;
