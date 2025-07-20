d89179d197a17d0be458b7a248539832c060ef45
Kacy Do <contact@kacydo.dev>
2025-07-20 22:20:05 +1000

    add styling for HeroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,8 +10,10 @@ export default function HeroSection() {
 				height={600}
 				alt='hero image'
 			/>
-			<article className='absolute'>
-				<h1>Great coffee made simple.</h1>
+			<article className='w-[32%] mx-16 space-y-6 absolute text-accent-content'>
+				<h1 className='text-7xl font-bold font-serif'>
+					Great coffee made simple.
+				</h1>
 				<p>
 					Start your mornings with the world’s best coffees. Try our expertly
 					curated artisan coffees from our best roasters delivered directly to
