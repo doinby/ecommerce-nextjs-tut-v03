22852d3bde0383047133e02b7d8ec2e054937ce6
Kacy Do <contact@kacydo.dev>
2025-07-20 20:31:15 +1000

    initial setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+	return (
+		<nav className='space-x-4'>
+			<Link href='/products'>Products</Link>
+			<Link href='/checkout'>Checkout</Link>
+		</nav>
+	);
+}
